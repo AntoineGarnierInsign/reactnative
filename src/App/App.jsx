ed3425e3cfcdd295dc5f88fe2d1e93bf4a4b8d5f
Antoine GARNIER <antoine.garnier@insign.fr>
2024-01-10 11:12:26 +0100

    Image, style, props

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
+import {Image, Text, SafeAreaView, View} from 'react-native';
+import Button from './components/ui/Button/Button';
+import style from './App.style';
 
 const App = props => {
-    return null;
+  return (
+    <SafeAreaView>
+      <View>
+        <Button bgColor="red" color="green">
+          <View style={style.appButtonView}>
+            <Image
+              style={style.appButtonImage}
+              source={require('./assets/ramyeon.png')}
+            />
+            <Text style={{...style.appButtonText, ...style.bold}}>Patate</Text>
+          </View>
+        </Button>
+        <Button bgColor="blue" color="yellow" children={<Text>Hello</Text>} />
+        <Button>
+          <Image
+            style={style.appButtonImage}
+            source={require('./assets/ramyeon.png')}
+          />
+          <Text style={style.appButtonText}>Patate2</Text>
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default App;
--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,16 +1,27 @@
-import React from "react";
-import { TouchableHighlight, View } from "react-native";
+import React from 'react';
+import {Text, TouchableHighlight, View} from 'react-native';
+import PropTypes from 'prop-types';
+import style from './Button.style';
 
 const Button = props => {
-    console.log(props);
-    
-    return (
-        <TouchableHighlight>
-            <View>
-                <Text>Benjamin</Text>
-            </View>
-        </TouchableHighlight>
-    );
+  console.log(props);
+
+  return (
+    <TouchableHighlight>
+      <View style={{...style.Button, backgroundColor: props.bgColor}}>
+        {props.children}
+      </View>
+    </TouchableHighlight>
+  );
 };
 
+Button.propTypes = {
+  children: PropTypes.any.isRequired,
+  bgColor: PropTypes.oneOf(['red', 'yellow']),
+  color: PropTypes.string,
+};
+Button.defaultProps = {
+  bgColor: 'red',
+  children: <Text>Button</Text>,
+};
 export default Button;
